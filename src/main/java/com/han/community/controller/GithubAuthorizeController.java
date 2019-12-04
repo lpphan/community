30cdee8da7f29331cc752f4c350cbff779724025
lpphan <hanzhishun1993>
2019-12-04 14:18:55 +0800

    更新

---
+++ src/main/java/com/han/community/controller/GithubAuthorizeController.java
@@ -0,0 +1,10 @@
+package com.han.community.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GithubAuthorizeController {
+    /**
+     * callback获取登录code
+     */
+}
