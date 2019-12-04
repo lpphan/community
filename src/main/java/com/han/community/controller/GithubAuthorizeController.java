3ab89e27e5ab62b60fe03707a6491986ac4c587d
lpphan <hanzhishun1993>
2019-12-04 14:26:45 +0800

    111

--- src/main/java/com/han/community/controller/GithubAuthorizeController.java
+++ src/main/java/com/han/community/controller/GithubAuthorizeController.java
@@ -1,10 +1,19 @@
 package com.han.community.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * github跳转登录
+ */
 @Controller
 public class GithubAuthorizeController {
     /**
      * callback获取登录code
      */
+    public String callBack(@RequestParam(name = "code")String code, @RequestParam(name = "status")String status, Model model){
+
+        return "index";
+    }
 }
