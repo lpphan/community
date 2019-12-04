c617c027de50838198c5409755e3ac8c11188b85
lpphan <lpphan@163.com>
2019-12-04 22:58:12 +0800

    得到github 用户信息

--- src/main/java/com/han/community/controller/GithubAuthorizeController.java
+++ src/main/java/com/han/community/controller/GithubAuthorizeController.java
@@ -1,10 +1,12 @@
 package com.han.community.controller;
 
 import com.han.community.dto.AccessTokenDTO;
+import com.han.community.dto.GithubUser;
 import com.han.community.util.GithubUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
@@ -24,15 +26,18 @@ public class GithubAuthorizeController {
     /**
      * callback获取登录code
      */
-    public String callBack(@RequestParam(name = "code")String code, @RequestParam(name = "status")String status, Model model) throws IOException {
+    @RequestMapping("/callback")
+    public String callBack(@RequestParam(name = "code")String code,
+                           @RequestParam(name = "state")String state, Model model) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(CLIENT_ID);
         accessTokenDTO.setClient_secret(CLIENT_SECRET);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(CALLBACK_URL);
-        accessTokenDTO.setState(status);
+        accessTokenDTO.setState(state);
         String accessToken = githubUtil.getAccessToken(accessTokenDTO);//得到access_token
-
+        GithubUser user = githubUtil.getUser(accessToken);
+        System.out.println(user);
         return "index";
     }
 }
--- src/main/java/com/han/community/util/GithubUtil.java
+++ src/main/java/com/han/community/util/GithubUtil.java
@@ -1,7 +1,9 @@
 package com.han.community.util;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.han.community.dto.AccessTokenDTO;
+import com.han.community.dto.GithubUser;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 
@@ -12,25 +14,44 @@ public class GithubUtil {
 
     private final String ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
 
+    private final String GET_GITHUB_USER_URL = "https://api.github.com/user?access_token=";
+
     /**
      * 获取access_token
      */
     public String getAccessToken(AccessTokenDTO accessTokenDTO) throws IOException {
-        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
         JSONObject jsonObject = new JSONObject();
-        RequestBody body = RequestBody.create(JSON, json);
+        RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO));
         Request request = new Request.Builder()
                 .url(ACCESS_TOKEN_URL)
                 .post(body)
                 .build();
         try (Response response = client.newCall(request).execute()) {
             String req = response.body().string();
-            System.out.println(req);
-            return response.body().string();
+            String[] split = req.split("&");
+            String s = split[0];
+            String[] split1 = s.split("=");
+            String access_token = split1[1];
+            System.out.println(access_token);
+            return access_token;
         }
-        return "";
     }
 
-
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(GET_GITHUB_USER_URL + accessToken)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            GithubUser user = JSON.parseObject(string, GithubUser.class);
+            System.out.println(user);
+            return user;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
